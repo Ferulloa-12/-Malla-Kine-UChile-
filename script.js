04aadd57d92c5211337e74e6d3c4b207351e959e
Ferulloa-12 <fernandaulloa145@gmail.com>
2025-07-23 16:20:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -139,6 +139,11 @@ function crearColumnaAnio(anioNumero, semestres) {
   columna.appendChild(contenedorSemestres);
   mallaContainer.appendChild(columna);
 }
+);
+
+// Crear columna del Año 1 (semestres 1 y 2)
+crearColumnaAnio(1, [1, 2]);
+
   // PARTE 2 – AÑO 2 (Semestres 3 y 4)
 ramos.push(
   {
